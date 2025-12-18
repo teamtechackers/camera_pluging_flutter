c808863d02b8878bbacd5f71ba1eed89067b2bfe
saimraza12 <saimrazasaimi@gmail.com>
2025-12-18 12:57:01 +0500

    change the version

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.app.ultrascan4d"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
         
         multiDexEnabled = true
         

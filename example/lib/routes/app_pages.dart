3d572d88c29075e5045e0fc4aa29fa7b81adf1fa
saimraza12 <saimrazasaimi@gmail.com>
2026-02-27 15:24:39 +0500

    resolve navigation transition issue

--- example/lib/routes/app_pages.dart
+++ example/lib/routes/app_pages.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 
-
 import '../core/api/models/analysis_response.dart';
 import '../features/body_area/presentation/body_area_home.dart';
 import '../features/result/presentation/result_page.dart';
@@ -18,9 +17,24 @@ class AppPages {
   static const resultPage = '/result-page';
 
   static final routes = [
-    GetPage(name: scanPage, page: () => const ScanPage()),
-    GetPage(name: cameraScreen, page: () => const CameraScreen()),
-    GetPage(name: initial, page: () => const SplashScreen()),
+    GetPage(
+      name: scanPage,
+      page: () => const ScanPage(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
+    ),
+    GetPage(
+      name: cameraScreen,
+      page: () => const CameraScreen(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
+    ),
+    GetPage(
+      name: initial,
+      page: () => const SplashScreen(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
+    ),
     GetPage(
       name: resultPage,
       page: () {
@@ -29,8 +43,20 @@ class AppPages {
             : throw ArgumentError('AnalysisResponse is required');
         return ResultPage(analysisResponse: analysisResponse);
       },
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
     ),
-    GetPage(name: settingPage, page: () => const SettingPage()),
-    GetPage(name: bodyAreaHome, page: () => const BodyAreaHome()),
+    GetPage(
+      name: settingPage,
+      page: () => const SettingPage(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
+    ),
+    GetPage(
+      name: bodyAreaHome,
+      page: () => const BodyAreaHome(),
+      transition: Transition.fadeIn,
+      transitionDuration: const Duration(milliseconds: 200),
+    ),
   ];
 }
